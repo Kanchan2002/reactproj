140b930104ee9342b59363da74e4c500943e95fa
Kanchan tripathi <95993754+Kanchan2002@users.noreply.github.com>
2023-07-24 20:10:11 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+function App() {
+  
+  return (
+      <div>
+         <h2>Expense item</h2>
+          <h1>food rs 10</h1>
+          <h1>petrol rs 100</h1>
+          <h1>movie rs 200</h1>
+      </div>
+      
+  );
+}
+
+export default App;
